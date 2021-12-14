19e7472722a76ded594e23556601477c82e7f3f2
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-12-15 00:59:56 +0100

    apollo server and gql setup

---
+++ index.js
@@ -0,0 +1,36 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+];
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
